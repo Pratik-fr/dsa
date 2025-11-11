95691b603afac5c32dee62ec3a9168c7890b1787
Pratik-fr <pratiktoruskar022@gmail.com>
2025-11-12 00:22:25 +0530

    basic math

--- basicmath.py
+++ basicmath.py
@@ -1,27 +1,58 @@
-#number od digits
-n=input("enter digit")
-n=str(n)
-print(len(n))
+# #number od digits
+# n=input("enter digit")
+# n=str(n)
+# print(len(n))
 
-#reverse number
+# #reverse number
+# n=int(input("enter num "))
+# rev=0
+# while n!=0:
+#     rem=n%10
+#     rev=rev*10+rem
+#     n=n//10
+# print(rev)
+
+# #number palindrome
+# n=int(input("enter num "))
+# rev=0
+# num=n
+# while n!=0:
+#     rem=n%10
+#     rev=rev*10+rem
+#     n=n//10
+# print(rev)
+# if num==rev:
+#     print("palindrome")
+# else: print("Not a palindrome")
+
+# #GCD
+# n=int(input("enter num "))
+# rev=0
+# num=n
+# while n!=0:
+#     rem=n%10
+#     rev+=rem**3
+#     n=n//10
+# if rev==num:
+#     print("true")
+# else:
+#     print('false')
+
+# #find all divisors
+# n=int(input("enter num "))
+# p=[]
+# for i in range(1,n+1):
+#     if n%i==0:
+#         p.append(i)
+# print(p)
+
+#check for prime
 n=int(input("enter num "))
-rev=0
-while n!=0:
-    rem=n%10
-    rev=rev*10+rem
-    n=n//10
-print(rev)
-
-#number palindrome
-n=int(input("enter num "))
-rev=0
-num=n
-while n!=0:
-    rem=n%10
-    rev=rev*10+rem
-    n=n//10
-print(rev)
-if num==rev:
-    print("palindrome")
-else: print("Not a palindrome")
-
+isprime=1
+for i in range(2,n//2+1):
+    if n%i==0:
+        isprime=0
+if isprime==1:
+    print("prime")
+else:
+    print('not prime')
