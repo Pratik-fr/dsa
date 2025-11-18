9976655c434d5276ffc71c11d077940ecf287a3e
Pratik-fr <pratiktoruskar022@gmail.com>
2025-11-19 01:15:17 +0530

    Recursion

--- recursion.py
+++ recursion.py
@@ -75,3 +75,14 @@ if p==s:
     print("palindrome")
 else:
     print("No")
+
+#fibonacci
+def fib_series(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+num = (10)
+fib_series(num)
+
+
